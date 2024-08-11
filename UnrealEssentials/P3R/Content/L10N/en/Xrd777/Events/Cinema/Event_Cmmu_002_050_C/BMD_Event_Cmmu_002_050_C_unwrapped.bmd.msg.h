3e911ce8b904755cdfb7c9af9d5c00444d7a2514
AerosTactician <a.shankland.19@gmail.com>
2024-08-11 19:52:58 +1000

    Missing Yukari SL Files Added.

---
+++ UnrealEssentials/P3R/Content/L10N/en/Xrd777/Events/Cinema/Event_Cmmu_002_050_C/BMD_Event_Cmmu_002_050_C_unwrapped.bmd.msg.h
@@ -0,0 +1,11 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int MSG_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MSG_002_5_0                      = 3;
+const int MND_003_0_0                      = 4;
+const int SEL_004_0_0                      = 5;
+const int MND_005_0_0                      = 6;
+const int SEL_006_0_0                      = 7;
+const int MND_007_0_0                      = 8;
+const int MND_008_0_0                      = 9;
